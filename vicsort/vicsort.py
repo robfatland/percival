358b96f9864668b8424816c50fce93e7b44f4097
robfatland <fatland@robfatland.net>
2015-10-31 23:34:12 -0700

    more mods to vicsort towards comparatives

--- vicsort/vicsort.py
+++ vicsort/vicsort.py
@@ -13,42 +13,120 @@ from os.path import isfile, join
 #   Set the Properties to include a nice time delay value like 8 or 16 days
 #   Right-click the layer and set the opacity to very faint to see the underlying terrain
 #
-# kilroy here are a few items that could be managed better:
+# kilroy here are a few items that could be done better or explored
 #   - by adding a lat and lon column to the output you can switch between column and marker representation;
 #     in fact WWT allows you to have both on: Think column with beacon at the top for starters. 
 #     This could facilitate comparison of two types, e.g. precip and evap. Also important would be a 
 #     lat/lon-only (no GEOMETRY) option.
-#   - drawing polygons CW versus CCW can have different effects...
-#   - other approaches to dual-data include basing alt and color on different columns and creating pairs of indicators in each
-#     cell (splitting the real estate) so that your eye could possibly pick up on the two effective surfaces.
-#   - solid color (no altitude coding) would also give a simpler way of comparing two types. This is a ball of worms
-#     but one approach is to make a reference color list and then choose a range in that list for a particular run
-#     where you have to take care to get the indexing / thresholding adjusting properly
+#   - drawing polygons CW versus CCW can have different effects... w/r/t LINESTRING / POLYGON
+#   - dual data: placing layers at 200k meters as the pedestal (with LINESTRING) may actually obviate the need for relief because
+#     at that height it just looks mostly like a shell where the color-coding is carrying most of the information. Anyway having
+#     the option to put something Way Up There is a nice way of creating a differentiation / comparison context.
+#   - unexplored idea: Make alt and color from different columns
+#   - create pairs of indicators in each cell (splitting the real estate) but I doubt this will work.
+#   - solid color (no altitude coding) would also give a simpler way of comparing two types. The easiest way to do this is in WWT
+#       . turn off the color column
+#       . right-click the layer and set a color for it (white by default)
 #   - give the program some command line arguments
-#   - pedestal should really be way high up for evap since it is + and -
+
+###################
+#
+# hardcoded configuration elements
+#
+###################
+
+# choose precip = 3, evap = 4, runoff = 5, baseflow = 6
+dataTypeIndex = 4
 
 # command line arguments; pulled up from 'buried in the code'
 # Data grab will commence on calendar year (1950 + yearsSkip)
-yearsSkip = 7
+yearsSkip = 0
+
 # we read every (linesDaySkip + 1)'th day of data and skip the remainder
 linesDaySkip = 2
+
 # This code only gets a year or two of data
-yearsGet = 5
-# place the data on a pedestal to get it up above most of the topography (6000 meters)
-pedestal = 6000.0
-# choose precip = 3, evap = 4, runoff = 5, baseflow = 6
-dataTypeIndex = 3
+yearsGet = 1
+
+# geographic grid spacing: hardcoded (kilroy) to be just under perfect
+dlat = 0.08
+dlon = 0.08
+
+# a little modifying factor for artistic license: Will expand/contract the basic cell dimensions
+latMag = 1.0
+lonMag = 1.0
+
 # turn this on (True) to drastically reduce number of polygons drawn
 drawOnlySignificantData = True
+
 # in the case where we don't want to draw "no precip" or "no runoff" or "no base flow" this threshold is used as the
 #   cutoff. It doesn't really make sense for evap; a different scheme would be wanted there (and a different pedestal)
 significanceThreshold = [0., 0., 0., 0.2, -10.0, 0.2, 0.01]
-pedestals = [0., 0., 0., 6000.0, 20000.0, 6000.0, 6000.0]
+
+# pedestals correspond in indices 3, 4, 5 and 6 to respectively precip, evap (+/-), runoff and base flow
+# They are altitudes in meters for displaying the data above the terrain in WWT
+pedestals = [0., 0., 0., 6000.0, 200000.0, 6000.0, 6000.0]
+
+# not used but anticipating choosing to put a shell version of a data type very high
+shellPedestals = [0., 0., 0., 200000.0, 200000.0, 200000.0, 200000.0]
+
+# component path and filename info
+rootdir = "C:\\Users\\fatla_000\\Documents\\"
+datadir = "Data\\"
+domaindir = "Jeff Richey Brazil DIF\\"
+qualifier = "fluxes"
+
+# This part is a bit circuitous but it works from data type selection (see up top).
+# We are pulling data from the list a[]: Column 3, 4, 5, or 6 (indexing from 0) 
+# corresponding to precip / evap / runoff / base flow. So you have to choose one
+# of those four indices: 3, 4, 5 or 6 as 'dataTypeIndex'. Everything follows from
+# there. One may wish to modify the intrinsic scaling.
+dataTypeScaleList = [0., 0., 0., 1000.0, 10000.0, 1000.0, 10000.0]
+dataTypeNameList = ['year', 'month', 'day', 'precip', 'evap', 'runoff', 'baseflow']
+
+# the color coding is based on the data column of interest (a3/a4/a5/a6) which becomes altitude
+# it *could* be done based on a different column than the altitude at the risk of being confusing
+colors = ['red','orange','green','cyan','blue','white']
+nColors = len(colors)
+colorIntervals = [0., 0., 0., 3000., 5000., 3000., 3000.]
+colorInterval = colorIntervals[dataTypeIndex]
 
 
 ########################
 #
-# Custom method for file I/O (Worldwide Telescope WKT formatting)
+# Derived values
+# 
+########################
+
+# choose between MULTIPOLYGON and MULTILINESTRING: At the moment tied to the data type chosen...
+geomType = 'MULTIPOLYGON'
+if dataTypeIndex == 4: geomType = 'MULTILINESTRING'
+
+# place the data on a pedestal to get it up above most of the topography
+pedestal = pedestals[dataTypeIndex]
+
+# For cases / indices 3, 5 and 6 the data are zero or positive so we simply work from the pedestal up.
+# For case / index 4 = evap the data are positive or negative so we set a floor based on seven colors
+floors = [0., 0., 0., pedestals[3], pedestals[4] - 3.0*colorIntervals[4], pedestals[5], pedestals[6]]
+floor = floors[dataTypeIndex]
+
+# used to calculate the color to use for a particular cell
+colorDC = floor
+
+# derived path info
+fullpathdir = rootdir + datadir + domaindir
+
+# The output file retains "type" (precip, runoff etc), "when" (year) and "how long" (in years) for this data subset
+# For example you could get vicsort_runoff_1964_2.csv which is data from 1964 and 1965 (2 years)
+outfileBase = 'vicsort_' + dataTypeNameList[dataTypeIndex] + '_' + str(1950 + yearsSkip) + '_' + str(yearsGet) + '_'
+
+# Get the data-to-altitude scale factor from a list of them
+dataTypeScale = dataTypeScaleList[dataTypeIndex]
+
+
+########################
+#
+# Custom methods
 #
 ########################
 
@@ -82,7 +160,7 @@ def WriteCSVFileWithGEOMETRY(file, longlist, header, dlonScale, dlatScale):
         b1 = str(longlist[start + 1] + dlatScale)
 
         # This is 'well known text' (WKT) and it draws a solid-color rectangle centered on the grid point
-        geomEntry = 'MULTIPOLYGON((' + a0 + ' ' + b0 + ',' + a1 + ' ' + b0 + ','
+        geomEntry = geomType + '((' + a0 + ' ' + b0 + ',' + a1 + ' ' + b0 + ','
         geomEntry += a1 + ' ' + b1 + ',' + a0 + ' ' + b1 + ',' + a0 + ' ' + b0 + ')),'
 
         f.write(geomEntry)
@@ -95,9 +173,10 @@ def WriteCSVFileWithGEOMETRY(file, longlist, header, dlonScale, dlatScale):
     # and that's it
     f.close()
 
-def extendRows(pedestal, value, dataTypeScale, colorDC, colorScale, nColors, lon, lat, year, month, day, colors, rows):
+# This appends the next entry onto a row: Done in different locations in the code so consolidated here
+def extendRows(pedestal, value, dataTypeScale, colorDC, colorInterval, nColors, lon, lat, year, month, day, colors, rows):
     altValue = pedestal + value*dataTypeScale
-    colorIndex = int((altValue - colorDC) / colorScale)
+    colorIndex = int((altValue - colorDC) / colorInterval)
     if colorIndex < 0: colorIndex = 0
     if colorIndex >= nColors: colorIndex = nColors - 1
     rows.extend([lon, lat, datetime(year, month, day), altValue, colors[colorIndex]])
@@ -108,14 +187,6 @@ def extendRows(pedestal, value, dataTypeScale, colorDC, colorScale, nColors, lon
 # Configure the vicsort parameters
 #
 ##############################
-
-
-# path information
-rootdir = "C:\\Users\\fatla_000\\Documents\\"
-datadir = "Data\\"
-domaindir = "Jeff Richey Brazil DIF\\"
-fullpathdir = rootdir + datadir + domaindir
-qualifier = "fluxes"
 
 # get all the filenames into the list all[]
 all = [ f for f in listdir(fullpathdir) if isfile(join(fullpathdir,f)) ]
@@ -130,17 +201,9 @@ lon = []
 # rows[] is populated with output CSV values which are eventually written to a file as WWT input
 rows = []
 
-# geographic grid spacing: hardcoded to be just under perfect
-dlat = 0.08
-dlon = 0.08
-
 # half that spacing
 dlat2 = dlat / 2.0
 dlon2 = dlon / 2.0
-
-# here is a little modifying factor for artistic license
-latMag = 1.0
-lonMag = 1.0
 
 # These values are actually used to size the rectangular blocks
 dlatScale = dlat2*latMag
@@ -151,32 +214,11 @@ outCounter = 0
 
 # bookkeeping
 linesSkip = int(yearsSkip * 365.25)
-linesGet = int(yearsGet*365.25)
+linesGet = int(yearsGet * 365.25)
 
 # This code only gets 1 in every (1 + linesDaySkip) days; typically 1 in 3
 linesDaysBlock = 1 + linesDaySkip
 readIterations = linesGet / linesDaysBlock
-
-# This part is a bit circuitous but it lets you select a data type fairly easily.
-# We are pulling data from the list a[]: Column 3, 4, 5, or 6 (indexing from 0) 
-# corresponding to precip / evap / runoff / base flow. So you have to choose one
-# of those four indices: 3, 4, 5 or 6 as 'dataTypeIndex'. Everything follows from
-# there but you may wish to modify the intrinsic scaling.
-dataTypeScaleList = [0., 0., 0., 1000.0, 100000.0, 1000.0, 10000.0]
-dataTypeNameList = ['year', 'month', 'day', 'precip', 'evap', 'runoff', 'baseflow']
-dataTypeScale = dataTypeScaleList[dataTypeIndex]
-
-# The output file retains "type" (precip, runoff etc), "when" (year) and "how long" (in years) for this data subset
-# For example you could get vicsort_runoff_1964_2.csv which is data from 1964 and 1965 (2 years)
-outfileBase = 'vicsort_' + dataTypeNameList[dataTypeIndex] + '_' + str(1950 + yearsSkip) + '_' + str(yearsGet) + '_'
-
-
-# the color coding is based on the data column of interest (a3/a4/a5) which becomes altitude
-# it *could* be done based on a different column than the altitude at the risk of being confusing
-colors = ['red','orange','green','cyan','blue','white']
-nColors = len(colors)
-colorDC = pedestal
-colorScale = 3000.0
 
 # create a list flux[] of files that begin with 'qualifier' which is 'fluxes'
 for a in all:
@@ -225,9 +267,9 @@ for i in range(len(flux)):
 
         if drawOnlySignificantData:
             if value > significanceThreshold[dataTypeIndex]:
-                extendRows(pedestal, value, dataTypeScale, colorDC, colorScale, nColors, lon[i], lat[i], int(a[0]), int(a[1]), int(a[2]), colors, rows)
+                extendRows(pedestal, value, dataTypeScale, colorDC, colorInterval, nColors, lon[i], lat[i], int(a[0]), int(a[1]), int(a[2]), colors, rows)
         else:
-            extendRows(pedestal, value, dataTypeScale, colorDC, colorScale, nColors, lon[i], lat[i], int(a[0]), int(a[1]), int(a[2]), colors, rows)
+            extendRows(pedestal, value, dataTypeScale, colorDC, colorInterval, nColors, lon[i], lat[i], int(a[0]), int(a[1]), int(a[2]), colors, rows)
 
     # show a pulse
     print "  ...did file " + flux[i]
