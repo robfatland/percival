cd293e95fa153b70bae7cc634a2fcc6d03ddd05b
robfatland <fatland@robfatland.net>
2015-11-06 16:18:31 -0800

    getting there on charm

--- shuffle/charm_shuffle.py
+++ shuffle/charm_shuffle.py
@@ -5,19 +5,6 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-#   5727 rows in the charm files (at most!)
-#
-# Forward scatter         (data)
-# Chlorophyll             (data)
-# Phycoerythrin           (data)
-#
-
-####################
-# 
-# Configure
-#
-####################
-
 rootdir = "C:\\Users\\fatla_000\\Documents\\"
 datadir = "Data\\"
 domaindir = "UW Oceanography\\Sophie\\"
@@ -28,15 +15,12 @@ outputBase = 'output'
 indexBase = 'index'
 
 
-# Get a list of all files in the folder
-all = [ f for f in listdir(path) if isfile(join(path,f)) ]
-
-# Get a list of all existing charm results files
+# Charm list
 charms = []
+all = [ f for f in listdir(path) if isfile(join(path,f)) ]
 for a in all:
     b = a.split('_')
     if b[0] == charmBase: charms.append(a)
-
 nCharms = len(charms)
 
 # The underway day-file list and the full underway header
@@ -67,200 +51,57 @@ while True:
     udata.append([day, file, l[7].rstrip('\n'), lon, lat, float(l[3]), float(l[4]), float(l[6])])
 
 f.close()
-
 nUnderway = len(udf)
+print 'Underway with ' + str(nUnderway) + ' rows\n'
 
-g = open(path + indexBase + '.csv')
-# no header to skip
+# To make this work we need to open all nCharms input files, open the index file, and creat the output file
+# The index file is the number of entries per underway row for each of the nCharms input files; sometimes zero
+f = open(path + indexBase + '.csv')
 
-for i in range(nUnderway):
-    line = g.readline()
-    index.append(line.split(',')[8:])
-    # This clears away that linefeed hanging on to the last element of the row in index[i]
-    index[i][nCharms - 1] = index[i][nCharms - 1].rstrip('\n')
-
-g.close()
-
-# Now I have to convert index[][] into integer values
-for i in range(nUnderway):
-    for j in range(nCharms):
-        index[i][j] = int(index[i][j])
-
-print '\n    ok we are underway with an index.\n\n'
-
+g = open(path + outputBase + '.csv', 'w')
 
 # set input[] to be a list of file objects
-input = []
+inputs = []
 for file in charms:
-    input.append(open(path + file))
+    inputs.append(open(path + file))
 
-# The output file
-f = open(path + outputBase + '.csv', 'w')
-
-
-# Let's loop over the nUnderway rows
 for i in range(nUnderway):
+    print '...row ' + str(i) + ' of total rows = ' + str(nUnderway)
 
-    # Let's append to udata[i], write it, and delete that memory
+    line = f.readline()              # next string from the index file
+    indexInfo = line.split(',')[8:]  # a list of nCharms strings as we skip the usual first 8 entries
+    if len(indexInfo) != nCharms:    # sanity check
+        print 'oh my'
+        sys.exit()
+    # enlightened: linefeed character at end of string does not impede int() type conversion
+    for j in range(nCharms):
+        indexInfo[j] = int(indexInfo[j])     # type convert to integers
 
     # Let's loop over the charm files that might possibly contribute to this row
     for j in range(nCharms):
-
-        if index[i][j] > 0:
-
-            r = input[i].readline()
-            s = r.split(',')
-            udata[i].extend(s[8:])
-
-    # udata[] is a list of lists.
-    # len(udata[i]) tells how many elements in the list which is the ith element of udata[] 
+        if indexInfo[j] > 0:
+            r = input[j].readline()
+            s = r.split(',')             # s is a list of strings with the unfortunate \n at the end of the last one maybe
+            udata[i].extend(r.split(',')[8:])
+    
     rowLen = len(udata[i])
+    sampLen = (rowLen - 8) / 3
 
-    # Let's print everything in the i'th list to the output file separated by commas
+    # ok now we change format: I'm going to pre-pend the number of triples in the row after the first 8 entries
+    udata.insert(8, sampLen)
+    rowLen += 1
+    
     for j in range(rowLen - 1):
-        f.write(str(udata[i][j]) + ',')
+        g.write(str(udata[i][j]) + ',')
     # ...with a trailing linefeed
-    f.write(str(udata[i][rowLen - 1]) + '\n')
+    g.write(str(udata[i][rowLen - 1]) + '\n')
 
     # This is intended to empty out the row that we just finished writing
     udata[i] = []
 
 f.close()
+g.close()
 for i in range(nCharms):
     input[i].close()
 
-## read rows from FC; append when possible to udata using udf to determine the index
-#while True:
 
-#    line = f.readline()
-#    if line == '': break
-#    counter += 1
-#    l = line.split(',')
-
-#    # some diagnostics on is it going ok?
-#    #if len(l) != fcArgsPerLine:
-#    #    numArgFailFC += 1
-#    #    if not argFailFC:
-#    #        argFailFC = True
-#    #        argFailFirstIndexFC = numLinesFC
-#    #    else:
-#    #        argFailLastIndexFC = numLinesFC
-
-#    # shout out every n'th
-#    if counter % modulusReportOut == 0: print counter, 'lines processed from FC'
-
-#    # julian day splits yyyy_ddd into year and day strings
-#    # avoiding this split in favor of more direct / faster...
-#    # jd = l[1].split('_')
-#    # day is pulled from yyyy_ddd in l[1] and file is pulled from l[2]
-#    # kilroy this looks dangerous if the day is only 1 or 2 digits
-#    # l[0] is the cruise ID; we ignore that here
-#    # day = int(l[1][-3:])
-#    # file = int(l[2])
-
-#    try: 
-#        hitIndex = udf.index((int(l[1][-3:]), int(l[2])))
-#    except ValueError:
-#        numDFFails += 1
-#    else:
-#        # you can append a triple tuple or you could extend 3 numbers
-#        udata[hitIndex].extend((float(l[3][:9]), float(l[4][:9]), float(l[5][:9])))
-#        nFCExtends += 1
-
-#    if counter >= numLinesToDo: break
-
-## At this point we've fallen out of the above while True with some number of appends to udata[]: 
-##   Either owing to trying out numLinesToDo rows of FC or ran out of FC data. In either case
-##   'counter' will indicate how many input FC rows we looked at. fcStartLine is the first row of
-##   fc we began with and nFCAppends is the number of successful appends from FC. Now we can figure
-##   out the proper name of the charm file. 
-#f.close()
-
-#charmFile = path + charmBase + '_' + str(fcStartLine + counter - 1) + '_' + str(fcStartLine) + '_' + str(nFCExtends) + '.csv'
-#f = open(charmFile, 'w')
-
-## Write out the harmonized output file
-#zeroDF = 0
-#nonzeroDF = 0
-#for i in range(len(udf)):
-#    if len(udata[i]) > underwayArgsPerLine:
-#        nonzeroDF += 1
-#        if writeOnlyFCAddedRows:
-#            # Write all of the elements of the row separated by commas...
-#            rowLen = len(udata[i])
-#            # For rowlen = 10 this loop goes 0, 1, 2, ..., 8
-#            for q in range(rowLen - 1):
-#                f.write(str(udata[i][q]) + ',')
-#            # ...and then we write element 9 with linefeed
-#            f.write(str(udata[i][rowLen - 1]) + '\n')
-#    else: 
-#        zeroDF += 1
-#        if not writeOnlyFCAddedRows:
-#            # Diagnostic just writes the lines as str(List)
-#            f.write(str(udata[i]))
-#            f.write('\n')
-
-#f.close()
-
-### histogram has 100 bins for 'thousands of points'
-##histogram = []
-##for i in range(100):
-##    histogram.append(0)
-
-##for i in range(len(udf)):
-##    bin = udata[i][8]/1000
-##    histogram[bin] += 1
-
-##for i in range(100):
-##    fHistogram.write(str(i*1000))
-##    fHistogram.write(',')
-##    fHistogram.write(str(histogram[i]))
-##    fHistogram.write('\n')
-                      
-##fHistogram.close() 
-
-
-
-
-###################
-######
-###### What happened...
-######
-###### All the post-run diagnostics are printed in this block
-######
-###################
-
-
-
-##print '\n'
-##print numLinesU, 'lines of data in Underway; arg count fail =', argFailU
-##if argFailU:
-##    print 'Underway first arg fail line =', argFailFirstIndexU
-##    print 'Underway last arg fail line =', argFailLastIndexU
-##    print 'Underway number of arg fails =', numArgFailU
-##print '\n'
-##print 'The datetime range in Underway is ', udata[0][2], 'to', udata[numLinesU-1][2]
-### print 'The interval is ', datetime.timedelta(datetime.datetime(udata[0][2]), datetime.datetime(udata[numLinesU-1][2]))
-##print '\n'
-##print 'day-file assignment fails = ', numDFFails, 'of attempts = ', numLinesFC
-##print '\n'
-##print 'min non-zero appends = ', minDFFC, 'and max non-zero appends = ', maxDFFC
-##print '\n'
-##print 'The last date-file pair in Underway is ', udf[numLinesU - 1][0], '--', udf[numLinesU - 1][1]
-##print 'The last date-file reached in the FC scan is ', maxDay, '--', maxFile
-##print '\n'
-
-
-##print 'expect', numLinesU, 'lines; totting up zero-appends and appends gives', zeroDF + nonzeroDF
-##print 'zero appends = ', zeroDF, 'and nonzero appends = ', nonzeroDF
-
-
-
-## print numLinesB, 'lines of data in flow cytometry (at least)'
-## print 'arg count fail =', argFailB
-## if argFailB:
-##     print 'first fail line =', argFailFirstIndexB
-##     print 'last fail line =', argFailLastIndexB
-##     print 'number of fails =', numArgFailB
-
-#print '\nI think I am done.\n'
