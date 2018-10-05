a5ef31232746a8e3a83a7f101e98aeb20e1ba292
robfatland <rob5@uw.edu>
2018-10-05 12:58:53 -0700

    clean house Oct 5

--- rob5araldif/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\fatla_000\Documents\Visual Studio 2013\Projects\percival\rob5araldif\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
